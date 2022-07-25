b2f56a9437479fb074539150bff18a6ea1830f99
Saral Srivastava <saralsrivastava25@gmail.com>
2022-07-25 23:12:06 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Store from "./pages/Store";
+import Cart from "./pages/Cart";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 
 const App = () => {
@@ -9,6 +15,21 @@ const App = () => {
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/account">
+            <Account />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
+          <Route exact path="/store/:storeID">
+            <Store />
           </Route>
         </Switch>
       </Router>
